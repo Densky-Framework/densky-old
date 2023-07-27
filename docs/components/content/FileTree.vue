9a4dc1cbcc67046bb1acc30f12a22197358ed090
Brayan-724 <bjqf0724@gmail.com>
2023-07-26 21:17:27 -0600

    feat(docs) make docs

---
+++ docs/components/content/FileTree.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <div class="wrap">
+    <TreeFolder :name="name">
+      <slot />
+    </TreeFolder>
+  </div>
+</template>
+
+<style scoped>
+.wrap {
+  padding: 1rem;
+  display: inline-block;
+  min-width: 200px;
+  border: solid 1px #666;
+  border-radius: 0.5rem;
+}
+
+.wrap > div {
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+}
+</style>
