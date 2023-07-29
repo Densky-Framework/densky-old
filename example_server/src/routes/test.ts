7a4a40d3d0c7edaebd049785d2778358d16dcfc3
Brayan-724 <bjqf0724@gmail.com>
2023-07-28 23:04:22 -0600

    example(update) Current features

--- example_server/src/routes/test.ts
+++ example_server/src/routes/test.ts
@@ -2,7 +2,7 @@ import { outFunction } from "../outFunction.ts";
 const externalVar = "World";
 
 export function GET() {
-  return new Response("Hello " + externalVar + outFunction(2, 2));
+  return new Response("Hello " + externalVar + outFunction(3, 2));
 }
 
 const completeText = "Hello " + externalVar;
